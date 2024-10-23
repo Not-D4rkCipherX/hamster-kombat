e844be04aa3561cafccdd2a654244f1de05151e9
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-23 13:35:17 +0700

    Update tapper.py

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -648,6 +648,8 @@ class Tapper:
                 }
                 self.http = HttpRequest(proxy, headers["User-Agent"])
                 logger.info(f"{self.session_name} | bind with proxy ip: {proxy}")
+            else:
+                self.http = HttpRequest(proxy, headers["User-Agent"])
         else:
             self.http = HttpRequest(proxy, headers["User-Agent"])
 
